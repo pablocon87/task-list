d86c13450ef71964da3711b3015e11b743645006
Pablo Conti <66192962+pablocon87@users.noreply.github.com>
2021-11-12 09:42:35 -0300

    Primer Commit

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { TaskService } from '../../service/task.service';
+import{Task} from '../../Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+/*tasks: Task[] =TASKS;*/
+tasks: Task[] = [];
+
+  constructor(
+    private taskService: TaskService
+  ) { }
+
+  ngOnInit(): void {
+      this.taskService.getTasks().subscribe((tasks)=>[
+        this.tasks=tasks
+      ]);
+  }
+
+}
